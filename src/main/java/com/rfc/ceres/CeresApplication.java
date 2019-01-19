9a556e0c12b593f058be3955b05fb19ac4937b2e
Raúl <raul.franco@protonmail.com>
2019-01-19 15:20:20 +0100

    Initial commit

---
+++ src/main/java/com/rfc/ceres/CeresApplication.java
@@ -0,0 +1,14 @@
+package com.rfc.ceres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeresApplication.class, args);
+	}
+
+}
+
