19bed2613667212fa91cd857b985f5b87849e727
Raúl <raul.franco@protonmail.com>
2019-05-21 21:40:06 +0200

    Adds user repository integration tests

--- src/main/java/com/rfc/ceres/user/UserRepository.java
+++ src/main/java/com/rfc/ceres/user/UserRepository.java
@@ -1,12 +1,12 @@
 package com.rfc.ceres.user;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
 	Optional<User> findByUserName(String username);
 }
--- src/test/java/com/rfc/ceres/user/UserRepositoryIntegrationTest.java
+++ src/test/java/com/rfc/ceres/user/UserRepositoryIntegrationTest.java
@@ -7,9 +7,10 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -22,22 +23,120 @@ public class UserRepositoryIntegrationTest {
 	private UserRepository userRepository;
 
 	@Test
-	public void whenFindByName_thenReturnUser() {
+	public void whenFindByUserName_thenReturnUser() {
 		// given
-		User user = new User();
-		user.setName("Marvin");
-		user.setEmail("marvin@protonmail.com");
-		user.setSurname("Langley");
-		user.setSecondSurname("Williams");
-		user.setUserName("Marvellous");
-		User savedUser = entityManager.persist(user);
+		User marvin = new User();
+		marvin.setName("Marvin");
+		marvin.setEmail("marvin@protonmail.com");
+		marvin.setSurname("Langley");
+		marvin.setSecondSurname("Williams");
+		marvin.setUserName("Marvellous");
+		User savedUser = entityManager.persist(marvin);
 		entityManager.flush();
 
 		// when
-		Optional<User> found = userRepository.findById(savedUser.getId());
+		Optional<User> foundUser = userRepository.findByUserName(savedUser.getUserName());
 
 		// then
-		assertThat(found.map(User::getName).isPresent());
+		assertTrue(foundUser.isPresent());
+		foundUser.ifPresent(user -> assertEquals("Marvin", user.getName()));
+		foundUser.ifPresent(user -> assertEquals("marvin@protonmail.com", user.getEmail()));
+		foundUser.ifPresent(user -> assertEquals("Langley", user.getSurname()));
+		foundUser.ifPresent(user -> assertEquals("Williams", user.getSecondSurname()));
+		foundUser.ifPresent(user -> assertEquals("Marvellous", user.getUserName()));
 	}
+
+	@Test
+	public void whenFindById_thenReturnUser() {
+		// given
+		User marvin = new User();
+		marvin.setName("Marvin");
+		marvin.setEmail("marvin@protonmail.com");
+		marvin.setSurname("Langley");
+		marvin.setSecondSurname("Williams");
+		marvin.setUserName("Marvellous");
+		User savedUser = entityManager.persist(marvin);
+		entityManager.flush();
+
+		// when
+		Optional<User> foundUser = userRepository.findById(savedUser.getId());
+
+		// then
+		assertTrue(foundUser.isPresent());
+		foundUser.ifPresent(user -> assertEquals("Marvin", user.getName()));
+		foundUser.ifPresent(user -> assertEquals("marvin@protonmail.com", user.getEmail()));
+		foundUser.ifPresent(user -> assertEquals("Langley", user.getSurname()));
+		foundUser.ifPresent(user -> assertEquals("Williams", user.getSecondSurname()));
+		foundUser.ifPresent(user -> assertEquals("Marvellous", user.getUserName()));
+	}
+
+	@Test
+	public void whenExistsById_thenReturnTrue() {
+		// given
+		User marvin = new User();
+		marvin.setName("Marvin");
+		marvin.setEmail("marvin@protonmail.com");
+		marvin.setSurname("Langley");
+		marvin.setSecondSurname("Williams");
+		marvin.setUserName("Marvellous");
+		User savedUser = entityManager.persist(marvin);
+		entityManager.flush();
+
+		// when
+		boolean userExists = userRepository.existsById(savedUser.getId());
+
+		// then
+		assertTrue(userExists);
+	}
+
+	@Test
+	public void whenSave_thenReturnUser() {
+		// given
+		User marvin = new User();
+		marvin.setName("Marvin");
+		marvin.setEmail("marvin@protonmail.com");
+		marvin.setSurname("Langley");
+		marvin.setSecondSurname("Williams");
+		marvin.setUserName("Marvellous");
+
+		// when
+		User savedUser = userRepository.save(marvin);
+
+		// then
+		assertEquals("Marvin", savedUser.getName());
+		assertEquals("marvin@protonmail.com", savedUser.getEmail());
+		assertEquals("Langley", savedUser.getSurname());
+		assertEquals("Williams", savedUser.getSecondSurname());
+		assertEquals("Marvellous", savedUser.getUserName());
+	}
+
+	@Test
+	public void whenSaveAll_thenReturnAllUsers() {
+		// given
+		User marvin = new User();
+		marvin.setName("Marvin");
+		marvin.setEmail("marvin@protonmail.com");
+		marvin.setSurname("Langley");
+		marvin.setSecondSurname("Williams");
+		marvin.setUserName("Marvellous");
+
+		User marvin2 = new User();
+		marvin2.setName("Marvin2");
+		marvin2.setEmail("marvin2@protonmail.com");
+		marvin2.setSurname("Langley2");
+		marvin2.setSecondSurname("Williams2");
+		marvin2.setUserName("Marvellous2");
+
+		var userList = List.of(marvin, marvin2);
+
+		// when
+		List<User> savedUsers = (List<User>) userRepository.saveAll(userList);
+
+		// then
+		assertFalse(savedUsers.isEmpty());
+		assertEquals(2, savedUsers.size());
+	}
+
+
 
 }
